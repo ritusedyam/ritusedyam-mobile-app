598f9f07d951a2c96d0683c6cae2afc17a588a2f
ritusedyam <ritusedyam@gmail.com>
2020-08-09 21:01:43 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, VERSION } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'my-app',
@@ -6,5 +7,18 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
+  public cropsCollection: Array<string> = [];
+
+  constructor(private db: AngularFirestore) {
+      const crops = db.collection("crops").valueChanges();
+      crops.subscribe((cropsList) => {
+        // console.log(cropsList);
+        cropsList.forEach((cropName) => {
+          this.cropsCollection.push(cropName["name"]);
+          console.log(cropName["name"]);
+        })
+      });
+  }
+
   name = 'Welcome to Ritusedyam';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,32 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+// 1. Import the libs you need
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+ const config = {
+    apiKey: "AIzaSyAcrCgKORiwCPaywZ5l059QfnML90PNtRw",
+    authDomain: "ritusedyam-mobile-app-77341.firebaseapp.com",
+    databaseURL: "https://ritusedyam-mobile-app-77341.firebaseio.com",
+    projectId: "ritusedyam-mobile-app-77341",
+    storageBucket: "ritusedyam-mobile-app-77341.appspot.com",
+    messagingSenderId: "427100545534",
+    appId: "1:427100545534:web:7a3dffc41ec274c3daa0af",
+    measurementId: "G-NEGSE9G2W4"
+  };
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ 
+    BrowserModule, 
+    FormsModule, 
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule // storage 
+    ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
