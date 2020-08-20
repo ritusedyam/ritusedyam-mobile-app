8a8d891754781c3f874a28be035bf3af476bbff2
ritusedyam <ritusedyam@gmail.com>
2020-08-20 19:57:04 +0530

    1 file modified, 1 added, 1 deleted

--- src/app/core/welcome/welcome.component.ts
+++ src/app/core/welcome/welcome.component.ts
@@ -13,6 +13,6 @@ export class WelcomeComponent {
   constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
     iconRegistry.addSvgIcon(
         'ritusedyam',
-        sanitizer.bypassSecurityTrustResourceUrl('app/assets/icons/ritusedyam.svg'));
+        sanitizer.bypassSecurityTrustResourceUrl('assets/icons/ritusedyam.svg'));
   }
 }
